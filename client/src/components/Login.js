71495578ed563cab3bca199b40b8523b81457d48
Noman <100629659+N02468@users.noreply.github.com>
2025-12-05 08:31:12 +0500

    Update login API endpoint to production URL

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post('https://user-authentication-and-authorization-6.onrender.com/api/auth/login', {
         email,
         password
       });
